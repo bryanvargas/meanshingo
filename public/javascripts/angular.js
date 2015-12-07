ed719e949e6e39d76acb1f62cd2578358b399ad3
Shingo Yabuki <bryanprsvar@gmail.com>
2015-12-07 18:33:12 -0300

    desarrollo front-end

---
+++ public/javascripts/angular.js
@@ -0,0 +1,25 @@
+angular.module('appTareas', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('alta', {
+                url: '/alta',
+                templateUrl: 'views/alta.html',
+                controller: 'ctrlAlta'
+            })
+            .state('editar', {
+                url: '/editar/{id}',
+                templateUrl: 'views/editar.html'
+            });
+        $urlRouterProvider.otherwise('alta');
+    })
+    .controller('ctrlAlta', function ($scope) {
+        $scope.tarea = {}
+        $scope.tareas = [];
+
+        $scope.agregar = function () {
+            $scope.tareas.push({
+                nombre: $scope.tarea.nombre,
+                prioridad: parseInt($scope.tarea.prioridad)
+            })
+        }
+    })
